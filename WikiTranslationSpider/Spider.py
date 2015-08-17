917f6aebf1e8ea3922da1389ef79a33bdfdb42d9
wgjak47 <ak47m61@gmail.com>
2015-08-17 22:25:07 +0800

    90%

--- WikiTranslationSpider/Spider.py
+++ WikiTranslationSpider/Spider.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
 import mwclient
 import json
+import time
+from mailthon import mail, postman
 
 class ConfigSpider():
     def __init__(self, config):
@@ -11,10 +13,15 @@ class ConfigSpider():
         self.Site = mwclient.Site(address,path)
 
     def filter(self, page):
-        pass
+        if page.pagelanguage == self.config['OriginLang']:
+            return True
+        return False
 
     def get_translation_page(self,OriginPages):
-        pass
+        TranslationPages = []
+        for page in OriginPages:
+            title = page.page_title
+            TranslationPages.append(self.Site.Pages[title+self.config['suffix']])
 
     def get_origin_page(self):
         OriginPages = []
@@ -23,20 +30,67 @@ class ConfigSpider():
                 self.OriginPages.append(page)
         return OriginPages
 
-    def get_status(self, OriginPages, TranslationPages):
-        pass
 
-    def sort_result(self, PagesToTranslate):
-        pass
+    def compare(self, o_page, t_page):
+        if not t_page.exists():
+            return True
+        o_page.text()
+        t_page.text()
+        for template in t_page.templates:
+            if template.page_title == unicode(self.config['template']):
+                t_page.tranlateme = True
+                return True
+            else:
+                t_page.tranlateme = False
+            time_from_change = time.mktime(o_page.last_rev_time) - \
+                time.mktime(t_page.last_rev_time)
+            if (time_from_change > 0):
+                t_page.time_from_change = time_from_change
+            return True
+
+
+    def get_status(self, OriginPages, TranslationPages):
+        PagesToTranslate = []
+        for o_page,t_page in zip(OriginPages,TranslationPages):
+            if self.compare(o_page,t_page):
+                PagesToTranslate.append(zip(o_page,t_page))
+        return PagesToTranslate
+
+    def sort_result(x, y):
+        o_page_x,t_page_x = x
+        o_page_y,t_page_y = y
+        if t_page_x.exists() and t_page_y.exists():
+            if t_page_x.tranlateme == True and t_page_y.tranlateme == True:
+                return t_page_x.time_from_change - t_page_y.time_from_change
+            elif t_page_x.tranlateme == True:
+                return 1
+            else:
+                return -1
+        elif t_page_x.exists():
+            return 1
+        else:
+            return -1
 
     def send_mail(self, PagesToTranslate):
-        pass
+        content = self.format_to_html(PagesToTranslate)
+        poster = postman(
+            host=self.config['mhost'],
+            auth=(self.config['muser'],self.config['mpasswd'])
+        )
+        result = poster.send(
+            content,
+            subject=self.config['Subject'],
+            sender=self.config['sender'],
+            receivers=self.config['receivers']
+        )
+        if not result.ok:
+            pass
 
     def main(self):
         OriginPages = self.get_origin_page()
         TranslationPages = self.get_translation_page(OriginPages)
         PagesToTranslate = self.get_status(OriginPages, TranslationPages)
-        self.sort_result(PagesToTranslate)
+        sorted(PagesToTranslate, self.sort_result)
         self.send_mail(PagesToTranslate)
 
 if __name__ == '__main__':
--- WikiTranslationSpider/page_sample.py
+++ WikiTranslationSpider/page_sample.py
@@ -1,10 +1,18 @@
+# coding=utf-8
 import mwclient
-site = mwclient.Site(('https','wiki.gentoo.org'),'/')
-page=site.Pages['Handbook:Main_Page']
+site = mwclient.Site(('https','wiki.archlinux.org'),'/')
+page=site.Pages['Main_page']
 print page.pagelanguage
-page=site.Pages['Handbook:Main_Page/zh_cn']
+page.text()
+print page.edit_time
+print page.last_rev_time
+page=site.Pages[u'List_of_Applications_(简体中文)']
+for i in page.templates():
+    print i.page_title
 print page.pagelanguage
-
-for i in site.Pages:
-    print i.pagelanguage
+page.text()
+print page.edit_time
+print page.last_rev_time
+#for i in site.Pages:
+#    print i.pagelanguage
 
