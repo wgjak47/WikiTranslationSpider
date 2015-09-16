b84ab6817c4f0527dd3710b26ad84f42131de620
wgjak47 <ak47m61@gmail.com>
2015-09-16 21:22:07 +0800

    修复安装错误。优化setup代码

--- setup.py
+++ setup.py
@@ -2,24 +2,29 @@
 # encoding=utf-8
 from setuptools import setup
 import os
+
 requirements = ['mwclient', 'gevent', 'markdown2']
+
 path = os.path.abspath(os.path.dirname(__file__))
+
 README = open(os.path.join(path, 'README.md')).read()
-setup(name='WikiTranslationBot',
-      version='0.8.0.dev1',  # Rember to also update __ver__ in client.py
-      description='MediaWiki API client',
-      long_description=README,
-      keywords='mediawiki spider translation',
-      author='wgjak47',
-      author_email='wgjak47@gmail.com',
-      entry_points={
-          'console_scripts':[
+
+setup(
+    name = 'WikiTranslationSpider',
+    version = '0.8.0.dev1',  # Rember to also update __ver__ in client.py
+    description = 'MediaWiki API client',
+    long_description = README,
+    keywords = 'mediawiki spider translation',
+    author = 'wgjak47',
+    author_email = 'wgjak47@gmail.com',
+    entry_points = {
+        'console_scripts': [
             'Spider = WikiTranslationSpider.Spider:main'
-          ]
-      },
-      url='https://github.com/wgjak47/WikiTranslationSpider',
-      license='MIT',
-      packages=['WikiTranslationSpider'],
-      install_requires=requirements,
-      zip_safe=True
-      )
+        ]
+    },
+    url = 'https://github.com/wgjak47/WikiTranslationSpider',
+    license = 'MIT',
+    packages = ['WikiTranslationSpider'],
+    install_requires = requirements,
+    zip_safe = False
+)
